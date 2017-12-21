a28c0acb790c4254d046db2228a82c2d5c2688d5
Yeimer Molina <yeimer.molina@gmail.com>
2017-12-21 18:15:32 -0400

    Add media component

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("HELLO")
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
+
+class App extends Component{
+    render(){
+        return (
+            <Media />
+        )
+    }
+}
+
+ReactDOM.render(<App />,document.getElementById('app'))
---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+    render(){
+        return (
+            <div>
+                <div>
+                    <img 
+                        src="" 
+                        alt=""
+                        width={260}
+                        height={160}
+                    />
+                    <h3>React</h3>
+                    <p>YeimerMlina</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Media;
