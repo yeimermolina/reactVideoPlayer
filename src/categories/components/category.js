4fe64e8fcf4d37c70898b6662909b575cce31a09
Yeimer Molina <yeimer.molina@gmail.com>
2017-12-26 18:55:10 -0400

    Open and close modal events

--- src/categories/components/category.js
+++ src/categories/components/category.js
@@ -9,6 +9,7 @@ function Category(props) {
       <h2 className="Category-title">{props.title}</h2>
       <Playlist
         playlist={props.playlist}
+        handleOpenModal={props.handleOpenModal}
       />
     </div>
   )
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -3,16 +3,43 @@ import HomeLayout from '../components/homeLayout';
 import Categories from '../../categories/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal';
+import Modal from '../../widgets/components/modal';
 
 class Home extends Component {
+
+  state = {
+    modalVisible: false
+  }
+
+  handleOpenModal = () => {
+    this.setState({
+      modalVisible: true
+    })
+  }
+
+  handleCloseModalClick = () => {
+    this.setState({
+      modalVisible: false
+    })
+  }
+
   render() {
     return (
       <HomeLayout>
         <Related />
-        <Categories categories={this.props.data.categories} />
-        <ModalContainer >
-          hola
+        <Categories 
+          categories={this.props.data.categories} 
+          handleOpenModal={this.handleOpenModal}
+        />
+        {this.state.modalVisible ? <ModalContainer >
+          <Modal 
+            handleClick={this.handleCloseModalClick}
+          >
+            <h1>Esto es un portal</h1>
+          </Modal>
         </ModalContainer>
+        : null
+      }
       </HomeLayout>
     )
   }
