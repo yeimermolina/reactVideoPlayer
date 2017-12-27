03f73724f92cb0afad6e81ad4f987e57779e07be
Yeimer Molina <yeimer.molina@gmail.com>
2017-12-27 15:17:50 -0400

    Add ref on input

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './search.css';
 
 const Search = (props) => (
-        <form className="Search">
+        <form 
+            className="Search"
+            onSubmit={props.handleSubmit}
+        >
             <input 
+                ref={props.setRef}
                 type="text"
                 placeholder="Buscar"
                 className="Search-input"
+                name="search"
+                // defaultValue="Luis"
+                onChange={props.handleInputChange}
+                value={props.value}
             />
         </form>
 )
