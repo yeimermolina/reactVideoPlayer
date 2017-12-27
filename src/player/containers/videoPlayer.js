8d3fcd709b6914f0329092e4e7796d9752a5270a
Yeimer Molina <yeimer.molina@gmail.com>
2017-12-27 16:02:22 -0400

    Add videoPlayer component and styles

--- src/player/containers/videoPlayer.js
+++ src/player/containers/videoPlayer.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/VideoPlayerLayout';
+import Video from '../components/video';
+import Title from '../components/title';
 
 class VideoPlayer extends Component {
     render(){
       return (
           <VideoPlayerLayout>
-              <video
-                controls
-                autoPlay
-                src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
+              <Title 
+                title="Prueba"
               />
-
+              <Video
+                 autoplay={true}
+                 src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+              />
           </VideoPlayerLayout>
       )  
     }
