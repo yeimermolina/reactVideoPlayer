c94a4ee2cb9c4ba0b0c4d090b1db5ec90b6ea3b4
Yeimer Molina <yeimer.molina@gmail.com>
2018-01-02 15:52:14 -0400

    Minor changes

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -6,6 +6,27 @@ function Related(props){
     return (
         <div className="Related">
             <img src={logo} alt="" width={250} />
+            <h3>New Videos</h3>
+            <ul>
+                <li>Video 1</li>
+                <li>Video 2</li>
+                <li>Video 3</li>
+                <li>Video 4</li>
+                <li>Video 5</li>
+                <li>Video 6</li>
+                <li>Video 7</li>
+            </ul>
+
+            <h3>Lorem ipsu</h3>
+            <ul>
+                <li>Video 1</li>
+                <li>Video 2</li>
+                <li>Video 3</li>
+                <li>Video 4</li>
+                <li>Video 5</li>
+                <li>Video 6</li>
+                <li>Video 7</li>
+            </ul>
         </div>
     );
 }
